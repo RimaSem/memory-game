15103a0005bc7ce2be494975cfa494d561edb5aa
RimaSem <rima.semetaite@gmail.com>
2023-02-12 15:31:14 +0200

    Create initial header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-title">
+        <h2>Game of Thrones</h2>
+        <h3>Memory Game</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
